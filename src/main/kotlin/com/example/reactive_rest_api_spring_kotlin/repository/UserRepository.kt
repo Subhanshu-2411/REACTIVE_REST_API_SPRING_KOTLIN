4464e8b33895e71cbcb237c63c17836064cb4688
Subhanshu Bansal <subhanshu20135@iiitd.ac.in>
2023-10-05 00:27:47 +0530

    Added Required Functions in User Repository

---
+++ src/main/kotlin/com/example/reactive_rest_api_spring_kotlin/repository/UserRepository.kt
@@ -0,0 +1,18 @@
+package com.example.reactive_rest_api_spring_kotlin.repository
+
+import com.example.reactive_rest_api_spring_kotlin.model.User
+import org.springframework.data.mongodb.repository.Query
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import org.springframework.stereotype.Repository
+import reactor.core.publisher.Flux
+
+@Repository
+interface UserRepository: ReactiveMongoRepository<User, Long?> {
+
+    fun findByNameContaining(name: String): Flux<User>
+
+    fun findByCompanyId(companyId: Long): Flux<User>
+
+    fun findByEmail(email: String): Flux<User>
+
+}
